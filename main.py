10fcfc78dafda20edf7cbc8859be9aeb1b2bbc9d
Erin Zhao <erinzhao8100@gmail.com>
2022-02-10 14:19:18 +0000

    completed lab

--- main.py
+++ main.py
@@ -19,8 +19,35 @@ leonardo.goto(-100,-20)
 
 ## 5. your code goes here
 
+# Race 1:
+michelangelo.forward(random.randint(1, 100))
+leonardo.forward(random.randint(1, 100))
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+#Race 2:
+for i in range(10):
+  michelangelo.forward(random.randint(1, 10))
+  leonardo.forward(random.randint(1, 10))
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+
 
 # Part B - complete part B here
 
+michelangelo.clear()
+#numSides is the number of sides of the polygon
+numSides = 6
+#deg is the degrees in each angle of polygon
+deg = 360 / numSides
+#sideLength is an arbitrary length of the sides. 50 was chosen so that the shape can be seen
+sideLength = 50
+
+michelangelo.down()
+
+#creates a regular polygon by iterating through movement of forward and turning. iterates through the number of sides
+for i in range(numSides):
+  michelangelo.forward(sideLength)
+  michelangelo.right(deg)
+michelangelo.up()
 
 window.exitonclick()
